3c22cafa21a6127af6cc5357dad0e1ade5df92c8
TomaszBeben <tbeben@st.swps.edu.pl>
2022-03-02 16:06:14 +0100

    urls and links

--- meetings/views.py
+++ meetings/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Meetings
+from .models import Meetings, Room
 
 def detail(req, id):
     meeting = get_object_or_404(Meetings, pk = id)
     return render(req, 'meetings/detail.html', {'meeting': meeting})
+
+def rooms_list(req):
+    return render(req, 'meetings/rooms_list.html', {'rooms': Room.objects.all()})
--- website/views.py
+++ website/views.py
@@ -5,14 +5,26 @@ from django.http import HttpResponse
 
 from datetime import datetime
 
-from meetings.models import Meetings
+from meetings.models import Meetings, Room
 
 def welcome(req):
     return render(req, 'website/welcome.html',
-                  {'num_meetings': Meetings.objects.count()})
+                    {
+                        'num_meetings': Meetings.objects.count(),
+                        'meetings_all': Meetings.objects.all(),
+                    }
+                    )
 
 def date(req):
     return HttpResponse('Page was served at: ' + str(datetime.now()))
 
 def about(req):
     return HttpResponse('YO! this is only django traning, keep going...')
+
+def rooms(req):
+    return render(req, 'website/rooms.html',
+                  {
+                      'num_rooms': Room.objects.count(),
+                      'rooms_all': Meetings.objects.all(),
+                  }
+                  )
